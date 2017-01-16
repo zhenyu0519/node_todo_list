ee6a1ef4fbf851bfc5b4e587735fd17e69ce56eb
zhenyu0519 <zhenyu051988@gmail.com>
2017-01-16 04:45:00 -0800

    angularjs and view done

---
+++ public/app.js
@@ -0,0 +1,35 @@
+//angular application
+var todoApp = angular.module('todoApp',[]);
+//The $http service requests a page on the server
+todoApp.controller('myCtrl', function($scope, $http){
+	$scope.formData = {};
+	//when lanuch on the pages, get all the todos and list them all
+	$http.get('/api/todos').suscess(function(data){
+		$scope.todos = data;
+		console.log(data);
+	}).error(function(data){
+		console.log('Error: ' + data);
+	});
+	//when submitting the add form, send text to the node API
+	$scope.createTodo = function(){
+		$http.post('api/todos', $scope.formData).success(function(data){
+			//clear the form so our user is ready to write other todo
+			$scope.formData = {};
+			$scope.todos = data;
+			console.log(data);
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+	//delete the todo after checking it
+	$scope.deleteTodo = function(id){
+		$http.detele('/api/todos/' + id).success(function(data){
+			$scope.todos = data;
+			console.log(data);
+		}).error(function(data){
+			console.log('Error ' + data);
+		});
+	};
+
+});
--- server.js
+++ server.js
@@ -12,13 +12,77 @@ var methodOverRide = require('method-override');
 var port = process.env.PORT || 8080;
 
 //connect with mongoDB
-mongoose.connect('');
+mongoose.connect('mongodb://todo:1234@jello.modulusmongo.net:27017/y9rawAsy');
 
 //set the static file path
 app.use(express.static(__dirname + '/public'));
 //use morgan to create log by the dev formant
 app.use(morgan('dev'));
 //use this instead of app.use(bodyParser()) which use the body parser
-app.use(bodyParser.urlencoded({extended:false}));
+//parse the Content-Type: application/x-www-form-urlencoded
+//extended is ture means using qs library to parser the url-encoded data 
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-//
+//parse application/vnd.api + json as json
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+//useOverRide middle ware
+app.use(methodOverRide());
+
+
+//define a model called todo and in this model there is string type text
+//which will contain our to do list content
+var todo = mongoose.model('todo',{
+	text:String
+});
+
+//restful api routes
+
+//list all todos
+app.get('/api/todos', function(req, res){
+	//use mongoose to get list all todos from database
+	todo.find(function(err,todos){
+		if(err){
+			//if there are any errors, send the error back
+			res.send(err);
+		}
+		//return all todos in JSON format
+		res.json(todo);
+	});
+});
+//create new todos
+app.post('/api/todos',function(req,res){
+	todo.create({
+		text: req.body.text,
+		done:false
+	}, function(err,todo){
+		if(err){
+			res.send(err);
+		}
+		todo.find(function(){
+			if(err){
+				res.send(err);
+			}
+			res.json(todos);
+		});
+	});
+});
+//delete todos
+app.delete('/api/todos',function(req,res){
+	tode.remove({
+		_id: req.params.todo_id
+	},function(err, todo){
+		if(err){
+			res.send(err);
+		}
+		res.json(todos);
+	});
+});
+
+//load the page view
+app.get('*',function(req,res){
+	res.sendfile('./public/index.html');
+});
+
+//listen the port
+app.listen(port);
+console.log("The server is running on " + port);
