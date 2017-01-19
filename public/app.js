670f99b5c7ee4e923dd44c32dba0482af47312ba
zhenyu0519 <zhenyu051988@gmail.com>
2017-01-18 23:13:16 -0800

    comments changed

--- public/app.js
+++ public/app.js
@@ -1,8 +1,9 @@
-//angular application
+//create angular application
 var todoApp = angular.module('todoApp',[]);
-//The $http service requests a page on the server
+//create the controller
 todoApp.controller('myCtrl', function($scope, $http){
-	$scope.formData = {};
+	//when lanuch the page, the text area should display the bind text as default
+	$scope.initForm = {};
 	//when lanuch on the pages, get all the todos and list them all
 	$http.get('/api/todos').success(function(todos){
 		$scope.todos = todos;
@@ -11,11 +12,11 @@ todoApp.controller('myCtrl', function($scope, $http){
 	});
 	//when submitting the add form, send text to the node API
 	$scope.createTodo = function(){
-		$http.post('api/todos', $scope.formData).success(function(todos){
-			//clear the form so our user is ready to write other todo
-			$scope.formData = {};
+		$http.post('api/todos', $scope.initForm).success(function(todos){
+			//clear the form to default text as our user is ready to write other todo
+			$scope.initForm = {};
 			$scope.todos = todos;
-			// console.log(todos);
+			console.log(todos);
 		}).error(function(todos){
 			console.log('Error: ' + todos);
 		});
@@ -23,7 +24,7 @@ todoApp.controller('myCtrl', function($scope, $http){
 
 
 
-	//delete the todo after checking it
+	//delete the todo with event id
 	$scope.deleteTodo = function(id){
 		console.log("the id is: " + id);
 		$http.delete('/api/todos/' + id).success(function(todos){
