26f027da4f983ce4e702b200bc5fc21ee3034d32
zhenyu0519 <zhenyu051988@gmail.com>
2017-01-18 20:27:14 -0800

    fixed the delete bugs

--- public/app.js
+++ public/app.js
@@ -4,21 +4,20 @@ var todoApp = angular.module('todoApp',[]);
 todoApp.controller('myCtrl', function($scope, $http){
 	$scope.formData = {};
 	//when lanuch on the pages, get all the todos and list them all
-	$http.get('/api/todos').success(function(data){
-		$scope.todos = data;
-		console.log("this is the " + data);
-	}).error(function(data){
-		console.log('Error: ' + data);
+	$http.get('/api/todos').success(function(todos){
+		$scope.todos = todos;
+	}).error(function(todos){
+		console.log('Error: ' + todos);
 	});
 	//when submitting the add form, send text to the node API
 	$scope.createTodo = function(){
-		$http.post('api/todos', $scope.formData).success(function(data){
+		$http.post('api/todos', $scope.formData).success(function(todos){
 			//clear the form so our user is ready to write other todo
 			$scope.formData = {};
-			$scope.todos = data;
-			console.log(data);
-		}).error(function(data){
-			console.log('Error: ' + data);
+			$scope.todos = todos;
+			// console.log(todos);
+		}).error(function(todos){
+			console.log('Error: ' + todos);
 		});
 	};
 
@@ -27,11 +26,11 @@ todoApp.controller('myCtrl', function($scope, $http){
 	//delete the todo after checking it
 	$scope.deleteTodo = function(id){
 		console.log("the id is: " + id);
-		$http.delete('/api/todos/' + id).success(function(data){
-			$scope.todos = data;
-			console.log(data);
-		}).error(function(data){
-			console.log('Error: ' + data);
+		$http.delete('/api/todos/' + id).success(function(todos){
+			$scope.todos = todos;
+			// console.log(todos);
+		}).error(function(todos){
+			console.log('Error: ' + todos);
 		});
 	};
 
